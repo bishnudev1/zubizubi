0f425d58b9996a2ab1e399269cdd07766aa2b4f5
bishnudev1 <bishnudevkhutia20@gmail.com>
2023-12-28 21:56:00 +0530

    Update dependencies and add Facebook login functionality

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,10 +1,8 @@
 import 'dart:developer';
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter/services.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:stacked/stacked.dart';
 import 'package:zubizubi/utils/toast.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class AuthServices with ListenableServiceMixin {
   Client client = Client()
@@ -12,36 +10,35 @@ class AuthServices with ListenableServiceMixin {
       .setProject('658bf6d7d729c67607f5')
       .setSelfSigned(status: true);
 
-  GoogleSignIn? googleSignIn;
-
   handleFacebookLogin() async {
     if (client == null) {
       showToast("Appwrite Client Initialization Failed");
       return;
     }
-    // try {
-    //   final res = await account.createOAuth2Session(
-    //     provider: 'facebook',
-    //   );
-    //   log('Facebook Login Response: $res');
-    // } on PlatformException catch (e) {
-    //   showToast(e.message.toString());
-    //   log('PlatformException: $e');
-    // } on AppwriteException catch (e) {
-    //   showToast(e.message.toString());
-    //   log('AppwriteException: $e');
-    // }
-    final LoginResult result = await FacebookAuth.instance
-        .login(); // by default we request the email and the public profile
-// or FacebookAuth.i.login()
-    if (result.status == LoginStatus.success) {
-      // you are logged
-      final AccessToken accessToken = result.accessToken!;
-      log(accessToken.toString());
-    } else {
-      log(result.status.toString());
-      log(result.message.toString());
+    try {
+      final account = Account(client);
+      final res = await account.createOAuth2Session(
+        provider: 'facebook',
+      );
+      log('Facebook Login Response: $res');
+    } on PlatformException catch (e) {
+      showToast(e.message.toString());
+      log('PlatformException: $e');
+    } on AppwriteException catch (e) {
+      showToast(e.message.toString());
+      log('AppwriteException: $e');
     }
+//     final LoginResult result = await FacebookAuth.instance
+//         .login(); // by default we request the email and the public profile
+// // or FacebookAuth.i.login()
+//     if (result.status == LoginStatus.success) {
+//       // you are logged
+//       final AccessToken accessToken = result.accessToken!;
+//       log(accessToken.toString());
+//     } else {
+//       log(result.status.toString());
+//       log(result.message.toString());
+//     }
   }
 
   handleGoogleLogin() async {
