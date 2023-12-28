39eef9cda982161953af05276e420d862637c856
bishnudev1 <bishnudevkhutia20@gmail.com>
2023-12-28 22:47:52 +0530

    Add flutter_animate dependency and update home_viewmodel.dart

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,30 +1,132 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:share_plus/share_plus.dart';
 import 'package:stacked/stacked.dart';
-import 'package:zubizubi/views/home/home_viewmodel.dart';
+import 'package:video_player/video_player.dart';
+
+import '../../data/models/video.dart';
+import 'home_viewmodel.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key, this.shareUrl}) : super(key: key);
   final String? shareUrl;
+  const HomeScreen({Key? key, this.shareUrl}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder.reactive(
       viewModelBuilder: () => HomeViewModel(),
+      createNewViewModelOnInsert: true,
+      onViewModelReady: (viewModel) async {
+        // await viewModel.initVideoPlayer();
+        log("shareUrl: $shareUrl");
+        if (shareUrl != null) {
+          log("shareUrl: $shareUrl");
+          await viewModel.parseShareUrl(shareUrl!);
+        }
+        await viewModel.init();
+      },
       builder: (context, viewModel, child) {
-        return Scaffold(
-            body: Center(child: Text("ZubiZubi-HomeScreen")),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                // viewModel.share();
-                viewModel.loginWithGoogle();
-              },
-              child: FaIcon(
-                FontAwesomeIcons.google,
-                color: Colors.white,
-              ),
-            ));
+        if (viewModel.isBusy) {
+          return const Scaffold(
+            backgroundColor: Colors.black,
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (viewModel.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text(viewModel.error.toString()),
+            ),
+          );
+        }
+        return SafeArea(
+            child: Scaffold(
+          backgroundColor: Colors.black,
+          // body: SizedBox.expand(child: feedVideos(viewModel)),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              viewModel.loginWithFacebook();
+            },
+            child: const FaIcon(FontAwesomeIcons.facebook),
+          ),
+        ),
+        
+        );
       },
     );
   }
+}
+
+Widget feedVideos(HomeViewModel viewModel) {
+  return PageView.builder(
+    controller: PageController(
+      initialPage: 0,
+      viewportFraction: 1,
+    ),
+    itemCount: viewModel.videoList!.length,
+    onPageChanged: (index) {
+      index = index % (viewModel.videoList!.length);
+      viewModel.changeVideo(index);
+    },
+    scrollDirection: Axis.vertical,
+    itemBuilder: (context, index) {
+      index = index % (viewModel.videoList!.length);
+      return videoCard(viewModel.videoList![index], context);
+    },
+  );
+}
+
+Widget videoCard(Video video, BuildContext context) {
+  return Stack(
+    children: [
+      video.controller != null
+          ? GestureDetector(
+              onTap: () {
+                if (video.controller!.value.isPlaying) {
+                  video.controller?.pause();
+                } else {
+                  video.controller?.play();
+                }
+              },
+              child: SizedBox.expand(
+                  child: FittedBox(
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: video.controller?.value.size.width ?? 0,
+                  height: video.controller?.value.size.height ?? 0,
+                  child: VideoPlayer(video.controller!),
+                ),
+              )),
+            )
+          : Container(
+              color: Colors.black,
+            ).animate(
+              onPlay: (controller) {
+                controller.repeat();
+              },
+            ).shimmer(
+              duration: const Duration(seconds: 2),
+              color: Theme.of(context).primaryColor.withOpacity(0.5),
+              curve: Curves.ease,
+            ),
+      Positioned(
+        right: 15,
+        bottom: 15,
+        child: IconButton(
+          onPressed: () {
+            // Share.share("https://j-aan.com/admin/share?url=${video.url}", subject: 'Check out this video!');
+          },
+          icon: const FaIcon(
+            FontAwesomeIcons.share,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ),
+    ],
+  );
 }
--- lib/views/home/home_viewmodel.dart
+++ lib/views/home/home_viewmodel.dart
@@ -1,8 +1,12 @@
 
 
+import 'dart:developer';
+
 import 'package:stacked/stacked.dart';
 import 'package:zubizubi/app/app.locator.dart';
+import '../../data/models/video.dart';
 import '../../services/auth_services.dart';
+import '../../services/video_services.dart';
 
 class HomeViewModel extends BaseViewModel{
   final _authServices = locator<AuthServices>();
@@ -13,5 +17,134 @@ class HomeViewModel extends BaseViewModel{
 
   loginWithGoogle() async {
     await _authServices.handleGoogleLogin();
+  }
+    List<Video>? videoList;
+
+  final _videoServices = locator<VideoServices>();
+
+  int currentIndex = 0;
+  int prevVideo = 0;
+
+  bool loading = false;
+
+  init() async{
+    await loadFetchNextBatchOfVideos();
+    // var initialVideoList = _videoServices
+    //     .demoVideoUrls()
+    //     .map<Video>((e) => Video(url: e, id: "123", likes: 32, videoTitle: "test"))
+    //     .toList();
+
+    // var random  = math.Random();
+
+    // initialVideoList.shuffle(random);
+
+    // if (videoList == null) {
+    //   videoList = initialVideoList;
+    // } else {
+    //   videoList!.addAll(initialVideoList);
+    // }
+    // loadFetchNextBatchOfVideos();
+
+    // auto play first video
+    loadVideo(0);
+    loadVideoInAdvance(1);
+  }
+
+  changeVideo(index) async {
+    if (videoList![prevVideo].controller != null) {
+      videoList![prevVideo].controller!.pause();
+
+
+      if (videoList![prevVideo].controller!.value.isInitialized) {
+        videoList![prevVideo].controller!.seekTo(Duration.zero);
+      }
+
+      if (index > prevVideo &&
+          prevVideo - 1 >= 0 &&
+          videoList![prevVideo - 1].controller != null &&
+          videoList![prevVideo - 1].controller!.value.isInitialized) {
+        log("disposing video index: ${prevVideo - 1}");
+        videoList![prevVideo - 1].dispose();
+      }
+
+      if (index < prevVideo &&
+          prevVideo + 1 < videoList!.length &&
+          videoList![prevVideo + 1].controller != null &&
+          videoList![prevVideo + 1].controller!.value.isInitialized) {
+        log("disposing video index: ${prevVideo + 1}");
+        videoList![prevVideo + 1].dispose();
+      }
+    }
+
+    if (videoList![index].controller == null) {
+      await videoList![index].loadController();
+    }
+    videoList![index].controller!.play();
+
+    //  next video in advance
+    if (index + 1 < videoList!.length) {
+      log("loading video in advance for ${index + 1}");
+      loadVideoInAdvance(index + 1);
+    }
+    if (index - 1 >= 0) {
+      log("loading video in advance for ${index - 1}");
+      loadVideoInAdvance(index - 1);
+    }
+
+    prevVideo = index;
+
+    // fetch next batch of videos
+    if (videoList!.length - 2 >= 0 &&
+        index >= videoList!.length - 2 &&
+        !loading) {
+      loadFetchNextBatchOfVideos();
+    }
+
+    notifyListeners();
+
+    log("Current index $index");
+  }
+
+  Future<void> loadFetchNextBatchOfVideos() async {
+    if (loading) return;
+    loading = true;
+    notifyListeners();
+
+    videoList ??= [];
+
+    var newVideos = await _videoServices.fetchVideosInBatches();
+    if (newVideos.length > 0) {
+      var newVideoList = newVideos
+          .map<Video>(
+              (e) => Video(url: e, id: "123", likes: 32, videoTitle: "test"))
+          .toList();
+      videoList!.addAll(newVideoList);
+    }
+
+    loading = false;
+    notifyListeners();
+  }
+
+  void loadVideo(int index) async {
+    if (videoList!.length > index) {
+      await videoList![index].loadController();
+      videoList![index].controller?.play();
+    }
+    notifyListeners();
+  }
+
+  void loadVideoInAdvance(int index) async {
+    if (videoList!.length > index && videoList![index].controller == null) {
+      await videoList![index].loadController();
+    }
+    notifyListeners();
+  }
+
+  parseShareUrl(String url) {
+    log("parseShareUrl called with $url");
+    videoList = [];
+    videoList!.add(Video(url: url, id: "123", likes: 32, videoTitle: "shared"));
+    loadVideo(0);
+    notifyListeners();
   }
 }
